1a74182ee3f1a29a66bf072aecd7826c40920e6a
Joel <itsjoelpm@gmail.com>
2022-09-14 17:52:34 +0300

    chore: init nav'

---
+++ components/Nav.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <>
+      <ul>
+        <li>new</li>
+        <li>threads</li>
+        <li>comments</li>
+        <li>show</li>
+        <li>submit</li>
+      </ul>
+
+      <div>
+        <h3>codingknite</h3>
+        <button>connect</button>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
