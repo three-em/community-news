3509fa31251840093590c877bf131fca5b3f9b95
Joel <itsjoelpm@gmail.com>
2022-09-15 12:36:34 +0300

    chore: connect arconnect

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import useArconnect from 'use-arconnect';
+import { PermissionType } from 'arconnect';
 
 const Nav = () => {
+  const arconnect = useArconnect();
+
+  const arConnectPermissions: PermissionType[] = [
+    'ACCESS_ADDRESS',
+    'ACCESS_ALL_ADDRESSES',
+    'SIGN_TRANSACTION',
+  ];
+
+  async function login() {
+    await window.arweaveWallet.connect(arConnectPermissions, {
+      name: 'CommunityLabs News',
+    });
+  }
+
   return (
     <>
       <ul>
@@ -13,7 +29,11 @@ const Nav = () => {
 
       <div>
         <h3>codingknite</h3>
-        <button>connect</button>
+        {(arconnect && <button onClick={login}>Connect</button>) || (
+          <button onClick={() => window.open('https://arconnect.io')}>
+            Install ArConnect
+          </button>
+        )}
       </div>
     </>
   );
