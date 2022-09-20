78dd5e1be90d1a934608fb95865464fca97c3b2f
Joel <itsjoelpm@gmail.com>
2022-09-20 22:36:17 +0300

    chore: add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
-import '../styles/globals.css';
+// import '../styles/globals.css';
 import Nav from '../components/Nav';
 import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyles />
       <Nav />
       <Component {...pageProps} />
     </>
