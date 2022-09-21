ccd8888383541cc0b898cb37e813d02b051da55b
Joel <itsjoelpm@gmail.com>
2022-09-21 07:41:10 +0300

    chore: short url

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,46 +1,26 @@
 import React from 'react';
+import * as utils from './utils';
 import * as Styled from './styles';
 
 interface PostProps {
   num: number;
   title: string;
-  shortUrl: string;
+  url: string;
   userPosted: string;
-  timeCreated: Date;
+  timeCreated: number;
   numberOfComments: number;
 }
 
 const Post = ({
-  num, // done
-  title, // done
-  shortUrl, // todo
+  num,
+  title,
+  url,
   userPosted, // todo
-  timeCreated, // do now
+  timeCreated,
   numberOfComments, // todo
 }: PostProps) => {
-  const getPostDate = (postDate: Date) => {
-    const currentDate = new Date();
-    const timeDiff = currentDate.getTime() - postDate.getTime(); // in milliseconds
-
-    let postTime = '';
-
-    // 1 minute = 60,000 milliseconds
-    // 1 hour = 3,600,000 milliseconds
-    // 1 day = 86,400,000 milliseconds
-
-    if (timeDiff >= 60000 && timeDiff < 3600000) {
-      postTime = `${Math.round(timeDiff / (1000 * 60))} minutes`;
-    } else if (timeDiff >= 3600000 && timeDiff < 86400000) {
-      postTime = `${Math.round(timeDiff / (1000 * 3600))} hours`;
-    } else if (timeDiff >= 86400000) {
-      postTime = `${Math.round(timeDiff / (1000 * 3600 * 24))} days`;
-    } else {
-      postTime = '1 miunte';
-    }
-
-    return postTime;
-  };
-  const timeSincePost = getPostDate(timeCreated);
+  const shortUrl = utils.getShortUrl(url);
+  const timeSincePost = utils.getPostDate(timeCreated);
 
   return (
     <Styled.PostWrapper>
