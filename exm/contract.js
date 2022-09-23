2ce8a2870dfb0ec9c530279f4bf53b74dff59204
Joel <itsjoelpm@gmail.com>
2022-09-24 00:25:38 +0300

    fix: refactor contract

--- exm/contract.js
+++ exm/contract.js
@@ -1,45 +1,12 @@
 export function handle(state, action) {
+  const { functionRole, postID, title, url, description, timeCreated, upvotes, walletAddress, author, userName } = action.input;
 
-  /**
-   *****POST FORMAT*****
-     const post = {
-      postID: '',
-      author: '',
-      title: '',
-      url: '',
-      description: '',
-      dateAndTime: '',
-      comments: [
-        {
-          author: '',
-          text: '',
-          replies: [
-            {
-              author: '',
-              text: '',
-            }
-          ]
-        }
-      ]
-
-    }
-   */
-
-
-  const { functionRole, postID, author, title, url, description, timeCreated, upvotes, username, comments, replies } = action.input;
+  if (functionRole === 'addUser') {
+    state.users.push({ walletAddress, userName });
+  };
 
   if (functionRole === 'createPost') {
     state.posts.unshift({ postID, author, title, url, description, upvotes, timeCreated });
   };
-
-  if (functionRole === 'addUsername') {
-    state.usernames.push({ username })
-  };
-
-  // this is strictly for dev. Do not deploy to prod
-  if (functionRole === 'clearPosts') {
-    state.posts.length = 0;
-  }
-
   return { state };
 }
