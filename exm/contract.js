7c2f91c5c7e82a9d3548bc78405bee66e05a6968
Joel <itsjoelpm@gmail.com>
2022-09-24 01:23:31 +0300

    fix: user enums

--- exm/contract.js
+++ exm/contract.js
@@ -1,12 +1,18 @@
 export function handle(state, action) {
+  const actions = {
+    ADD_USER: 'addUser',
+    CREATE_POST: 'createPost'
+  }
+
   const { functionRole, postID, title, url, description, timeCreated, upvotes, walletAddress, author, userName } = action.input;
 
-  if (functionRole === 'addUser') {
+  if (functionRole === actions.ADD_USER) {
     state.users.push({ walletAddress, userName });
   };
 
-  if (functionRole === 'createPost') {
+  if (functionRole === actions.CREATE_POST) {
     state.posts.unshift({ postID, author, title, url, description, upvotes, timeCreated });
   };
+
   return { state };
 }
