afba9130e57bf198dc4865319df49c030827cb45
Joel <itsjoelpm@gmail.com>
2022-10-04 05:33:53 +0300

    chore: refactor post component

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
 import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { useGettUser } from '../../hooks/useGettUser';
+import { ThumbsupIcon, NorthStarIcon } from '@primer/octicons-react';
+import { useGetAllData } from '../../hooks/useGetAllData';
 import * as utils from '../../utils/utils';
 import * as Styled from './styles';
-import { ThumbsupIcon } from '@primer/octicons-react';
 
 interface PostComponentProps {
   num: number;
@@ -10,6 +12,7 @@ interface PostComponentProps {
   url: string;
   postId: string;
   userPosted: string;
+  upvotes: number;
   timeCreated: number;
   numberOfComments: number;
 }
@@ -20,31 +23,92 @@ const Post = ({
   url,
   postId,
   userPosted,
+  upvotes,
   timeCreated,
   numberOfComments, // todo
 }: PostComponentProps) => {
-  const shortUrl = utils.getShortUrl(url);
-  const timeSincePost = utils.getPostDate(timeCreated);
+  const [showThumbsup, setShowThumbsup] = useState(false),
+    shortUrl = utils.getShortUrl(url),
+    timeSincePost = utils.getPostDate(timeCreated),
+    { userName } = useGettUser().currentUser,
+    { users } = useGetAllData();
+
+  useEffect(() => {
+    const userUpvotes = users
+      .map((user) => {
+        if (user.userName === userName) {
+          return user.upvotedPosts;
+        }
+      })
+      .flat();
+    const doesUpvoteExist = userUpvotes.some((upvote) => upvote === postId);
+    setShowThumbsup(doesUpvoteExist);
+  }, [postId, users, userName]);
+
+  const handlevote = async (action: 'upVote' | 'downVote') => {
+    await fetch('/api/upvote', {
+      method: 'POST',
+      body: JSON.stringify({
+        input: {
+          functionRole: action,
+          postID: postId,
+          userName,
+        },
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  };
 
   return (
     <Styled.PostWrapper>
       <Styled.PostInfo>
         <p>{num}.</p>
-        <ThumbsupIcon size={12} />
+
+        {userName === userPosted ? (
+          <NorthStarIcon size={10} />
+        ) : (
+          <>
+            {showThumbsup ? null : (
+              <div
+                onClick={() => {
+                  handlevote('upVote');
+                  setShowThumbsup(false);
+                }}
+              >
+                <ThumbsupIcon size={12} />
+              </div>
+            )}
+          </>
+        )}
       </Styled.PostInfo>
 
       <Styled.PostStats>
         <p className='title'>
           {title} ({shortUrl})
         </p>
-        <p>
-          posted by {userPosted} {timeSincePost} ago | 
-        </p>
-        <p>unvote |</p>
-        <p>hide |</p>
-        <Link href={`/post/${postId}`}>
-          {numberOfComments > 0 ? `${numberOfComments} comments` : 'discuss'}
-        </Link>
+        <div className='authorInfo'>
+          <p>
+            {upvotes} upvotes | posted by{' '}
+            <a href={`/user/${userPosted}`}>{userPosted}</a> {timeSincePost} ago
+            |
+          </p>
+          {showThumbsup ? (
+            <p
+              onClick={() => {
+                setShowThumbsup(false);
+                handlevote('downVote');
+              }}
+            >
+              unvote |
+            </p>
+          ) : null}
+          <p>hide |</p>
+          <Link href={`/post/${postId}`}>
+            {numberOfComments > 0 ? `${numberOfComments} comments` : 'discuss'}
+          </Link>
+        </div>
       </Styled.PostStats>
     </Styled.PostWrapper>
   );
