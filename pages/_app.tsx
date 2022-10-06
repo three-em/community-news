a11d086f864d68172d33f05beb3b4f871a51a78d
Joel <itsjoelpm@gmail.com>
2022-10-06 05:37:07 +0300

    chore: add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,19 @@ import { createGlobalStyle } from 'styled-components';
 import { CurrentUserProvider } from '../reducers/userContext';
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'NotoSansMono';
+    src: url('../assets/fonts/NotoSansMono.ttf') format('truetype');
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Courier New', Courier, monospace;
   }
+
+  
 `;
 
 function MyApp({ Component, pageProps }: AppProps) {
